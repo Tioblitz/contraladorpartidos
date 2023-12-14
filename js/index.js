a05c9bc09353fdfc727ee65b31bc6911ec7c3686
Felipe Carrasco P <carrascopatelli.fa@gmail.com>
2023-12-14 00:29:24 -0300

    Versión 1.2 - Agregando una base de datos y filtrando nuestros datos.

--- js/index.js
+++ js/index.js
@@ -1,50 +1,38 @@
-let lista = [1234567, 7654321, 13579246];
-
-function comprobar() {
-    let rut = Number(prompt("Ingrese rut del jugador (sin puntos ni digito verificador):"));
-    
-    if (rut == 0) {
-        return;
-    }
-    else if (isNaN(rut)) {
-        alert("El dato ingresado no es un número.");
-        return;
-    }
-    for (let i = 0; i < lista.length; i++) {
-        if (rut == lista[i]) {
-            alert("Jugador rut " + rut + " existe en nuestra base de datos.");
-            return;
-        }
-        else {
-            continue;
-        }
-    }
-    
-    alert("Dato no encontrado.");
+function mostrarJugadores() {
+    // let padre = document.querySelector(".lista-jugadores");
+    Jugadores.forEach((jugador) => {
+        /* padre.innerHTML += `
+            <li>${jugador.nombre} ${jugador.apellido} - Equipo: ${jugador.equipo} - Serie: ${jugador.serie}</li>
+        `
+         */
+        console.log(`[${jugador.serie}][${jugador.equipo}] ${jugador.nombre} ${jugador.apellido}`)
+    });
 }
 
-function agregar() {
-    let dato = Number(prompt("Ingrese un rut"));
-    if (isNaN(dato)) {
-        alert("El dato ingresado no es un número.");
-        return;
-    }
-    else if ((dato.length < 7 || dato.length > 8 )) {
-        alert("El dato ingresado (" + dato + ") no es un rut válido.");
-        return;
-    }
-    else {
-        for (let i = 0; i < lista.length; i++) {
-            if (dato == lista[i]) {
-                alert("Este dato ya está ingresado en nuestra base de datos.");
-                return;
-            }
-            else {
-                continue;
-            }
-        }
+function Filtrar() {
+    let nombre = prompt("Ingrese un nombre:");
+    let apellido = prompt("Ingrese un apellido:");
+    let equipo = prompt("Ingrese equipo (Caleuche/Bocheros):");
+    let serie = prompt("Ingrese serie (Junior/Senior):");
 
-        lista.push(dato);
-        alert("Nuevo dato agregado.");
+    nombre = nombre ? nombre.toLowerCase() : '';
+    apellido = apellido ? apellido.toLowerCase() : '';
+    equipo = equipo ? equipo.toLowerCase() : '';
+    serie = serie ? serie.toLowerCase() : '';
+
+    let resultado = Jugadores.filter(jugador => 
+        (nombre === '' || jugador.nombre.toLowerCase().includes(nombre)) &&
+        (apellido === '' || jugador.apellido.toLowerCase().includes(apellido)) &&
+        (equipo === '' || jugador.equipo.toLowerCase() === equipo) &&
+        (serie === '' || jugador.serie.toLowerCase() === serie)
+    );
+
+    if (resultado.length > 0) {
+        resultado.forEach((persona) => 
+            console.log(`[${persona.serie}][${persona.equipo}] ${persona.nombre} ${persona.apellido}`)
+        );
+    } else {
+        console.log("No se encontraron jugadores.");
     }
 }
+
